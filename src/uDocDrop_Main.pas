63b5d004e8609312bb4b4d9b6838a551f754384d
David Hein <david-hein196@web.de>
2018-08-11 21:13:21 +0200

    Add tab for moving rules Add first rule processing logic

--- src/uDocDrop_Main.pas
+++ src/uDocDrop_Main.pas
@@ -4,12 +4,12 @@ interface
 
 uses
   uDocDrop_Settings,
-  System.IniFiles,
+  System.IniFiles, System.IOUtils,
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   FMX.ListView.Types, FMX.ListView.Appearances, FMX.ListView.Adapters.Base,
   FMX.Controls.Presentation, FMX.StdCtrls, FMX.ListView, FMX.Layouts,
-  FMX.ListBox, FMX.TabControl;
+  FMX.ListBox, FMX.TabControl, FMX.Edit;
 
 type
   TFrmDocumentDropper = class(TForm)
@@ -27,6 +27,10 @@ type
     SaveDialog: TSaveDialog;
     OpenDialogLoad: TOpenDialog;
     btnLoadSelection: TButton;
+    btnProcess: TButton;
+    tbitmRules: TTabItem;
+    lstbxRules: TListBox;
+    edtRule: TEdit;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure btnSrcAddFilesClick(Sender: TObject);
@@ -38,6 +42,11 @@ type
     procedure lstbxDestinationsKeyDown(Sender: TObject; var Key: Word;
       var KeyChar: Char; Shift: TShiftState);
     procedure btnLoadSelectionClick(Sender: TObject);
+    procedure btnProcessClick(Sender: TObject);
+    procedure edtRuleKeyDown(Sender: TObject; var Key: Word; var KeyChar: Char;
+      Shift: TShiftState);
+    procedure lstbxRulesKeyDown(Sender: TObject; var Key: Word;
+      var KeyChar: Char; Shift: TShiftState);
   private
     { Private-Deklarationen }
     mSettings: TframeDocDropSettings;
@@ -55,6 +64,48 @@ var
 implementation
 
 {$R *.fmx}
+function Matchstrings(Source, pattern: string): Boolean;
+var
+  pSource: array [0..255] of Char;
+  pPattern: array [0..255] of Char;
+
+  function MatchPattern(element, pattern: PChar): Boolean;
+
+    function IsPatternWild(pattern: PChar): Boolean;
+    var
+      t: Integer;
+    begin
+      Result := StrScan(pattern, '*') <> nil;
+      if not Result then Result := StrScan(pattern, '?') <> nil;
+    end;
+  begin
+    if 0 = StrComp(pattern, '*') then
+      Result := True
+    else if (element^ = Chr(0)) and (pattern^ <> Chr(0)) then
+      Result := False
+    else if element^ = Chr(0) then
+      Result := True
+    else
+    begin
+      case pattern^ of
+        '*': if MatchPattern(element, @pattern[1]) then
+            Result := True
+          else
+            Result := MatchPattern(@element[1], pattern);
+          '?': Result := MatchPattern(@element[1], @pattern[1]);
+        else
+          if element^ = pattern^ then
+            Result := MatchPattern(@element[1], @pattern[1])
+          else
+            Result := False;
+      end;
+    end;
+  end;
+begin
+  StrPCopy(pSource, Source);
+  StrPCopy(pPattern, pattern);
+  Result := MatchPattern(pSource, pPattern);
+end;
 
 procedure TFrmDocumentDropper.btnDestAddDirClick(Sender: TObject);
 var
@@ -75,8 +126,80 @@ begin
     try
       FillListBox(lstbxSources, lIniFile.ReadString('Sources', 'Pathes', ''));
       FillListBox(lstbxDestinations, lIniFile.ReadString('Destinations', 'Pathes', ''));
+      FillListBox(lstbxRules, lIniFile.ReadString('Rules', 'Rules', ''));
     finally
       lIniFile.Free();
+    end;
+  end;
+end;
+
+procedure TFrmDocumentDropper.btnProcessClick(Sender: TObject);
+var
+  lSource: string;
+  lFiles: TStringDynArray;
+  lFile: string;
+  procedure CheckRules(const pFile: string);
+  var
+    lFile: string;
+    lRule: string;
+    lDest: string;
+    lRuleSrc: string;
+    lRuleDest: string;
+    lRuleSrcDir: string;
+    lRuleDestDir: string;
+    lPos: Integer;
+    lLength: Integer;
+    lDirectories: TStringDynArray;
+    lDirectory: string;
+  begin
+    for lRule in lstbxRules.Items do
+    begin
+      for lDest in lstbxDestinations.Items do
+      begin
+        lPos := Pos('->', lRule);
+        lLength := Length('->');
+        lDirectories := TDirectory.GetDirectories(lDest);
+
+        if lPos = 0 then
+          lRuleSrc := Trim(lRule)
+        else
+          lRuleSrc := Trim(Copy(lRule, 1, lPos - 1));
+        if lPos <> 0 then
+          lRuleDest := Trim(Copy(lRule, lPos + lLength, Length(lRule) - lPos))
+        else
+          lRuleDest := '';
+
+        for lDirectory in lDirectories do
+        begin
+          lFile := ExtractFileName(pFile);
+          lRuleSrcDir := StringReplace(lRuleSrc, '%dest%', ExtractFileName(lDirectory), [rfIgnoreCase, rfReplaceAll]);
+          lRuleDestDir := StringReplace(lRuleDest, '%dest%', ExtractFileName(lDirectory), [rfIgnoreCase, rfReplaceAll]);
+
+          if Matchstrings(lFile, lRuleSrcDir) then
+          begin
+            TFile.Move(pFile, IncludeTrailingBackslash(lDirectory) + lFile);
+            Exit();
+          end;
+        end;
+      end;
+    end;
+  end;
+begin
+  for lSource in lstbxSources.Items do
+  begin
+    if ExtractFileExt(lSource) = '' then
+    begin
+      // Directory
+      lFiles := TDirectory.GetFiles(lSource);
+      for lFile in lFiles do
+      begin
+        CheckRules(lFile);
+      end;
+    end
+    else
+    begin
+      // File
+      CheckRules(lSource);
     end;
   end;
 end;
@@ -91,6 +214,7 @@ begin
     try
       lIniFile.WriteString('Sources', 'Pathes', GetListBoxToString(lstbxSources));
       lIniFile.WriteString('Destinations', 'Pathes', GetListBoxToString(lstbxDestinations));
+      lIniFile.WriteString('Rules', 'Rules', GetListBoxToString(lstbxRules));
     finally
       lIniFile.Free();
     end;
@@ -114,6 +238,16 @@ begin
   begin
     for lFile in OpenDialogFiles.Files do
       lstbxSources.Items.Add(lFile);
+  end;
+end;
+
+procedure TFrmDocumentDropper.edtRuleKeyDown(Sender: TObject; var Key: Word;
+  var KeyChar: Char; Shift: TShiftState);
+begin
+  if Key = Ord(#13) then
+  begin
+    lstbxRules.Items.Add(edtRule.Text);
+    edtRule.Text := '';
   end;
 end;
 
@@ -155,6 +289,14 @@ begin
   if Key = Ord(#8) then
     if lstbxDestinations.ItemIndex <> -1 then
       lstbxDestinations.Items.Delete(lstbxDestinations.ItemIndex);
+end;
+
+procedure TFrmDocumentDropper.lstbxRulesKeyDown(Sender: TObject; var Key: Word;
+  var KeyChar: Char; Shift: TShiftState);
+begin
+  if Key = Ord(#8) then
+    if lstbxRules.ItemIndex <> -1 then
+      lstbxRules.Items.Delete(lstbxRules.ItemIndex);
 end;
 
 procedure TFrmDocumentDropper.lstbxSourcesKeyDown(Sender: TObject; var Key: Word;
