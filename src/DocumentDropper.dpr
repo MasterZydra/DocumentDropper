12d706f5a253bbd43243995d1064b71ad7a31999
David Hein <david-hein196@web.de>
2018-09-08 20:40:04 +0200

    Increase Build

--- src/DocumentDropper.dpr
+++ src/DocumentDropper.dpr
@@ -4,7 +4,6 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   uDocDrop_Main in 'uDocDrop_Main.pas' {FrmDocumentDropper},
-  uDocDrop_Settings in 'uDocDrop_Settings.pas' {frameDocDropSettings: TFrame},
   uDocDrop_Utils in 'uDocDrop_Utils.pas',
   uRuleProcessor in 'uRuleProcessor.pas';
 
